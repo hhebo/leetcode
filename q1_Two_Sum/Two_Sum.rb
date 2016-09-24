f818de9e3e17cc8146db37901e44dc0e6324dfd5
Tmac-15 <tmaclimt1@gmail.com>
2016-09-25 00:01:59 +0800

    update format

--- q1_Two_Sum/Two_Sum.rb
+++ q1_Two_Sum/Two_Sum.rb
@@ -2,18 +2,18 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-    result = []
-    hash = {}
-    hash.store(nums[0], 0)
-    for i in 1...nums.length
-        temp = target - nums[i]
-        if hash.has_key?(temp)
-            result << hash[temp]
-            result << i
-            return result
-        else
-            hash.store(nums[i], i)
-        end
-    end
-    result
+  result = []
+  hash = {}
+  hash[nums[0]] = 0
+  (1...nums.length).each do |i|
+      temp = target - nums[i]
+      if hash.has_key?(temp)
+          result << hash[temp]
+          result << i
+          return result
+      else
+          hash[nums[i]] = i
+      end
+  end
+  result
 end
