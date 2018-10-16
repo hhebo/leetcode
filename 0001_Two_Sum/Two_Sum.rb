1527157cdcec1f26e366f07acdcf8f5826f5bd6e
hhebo <hhebo@hhebo.com>
2018-10-16 10:27:55 +0800

    rename

---
+++ 0001_Two_Sum/Two_Sum.rb
@@ -0,0 +1,19 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  result = []
+  hash = {}
+  hash[nums[0]] = 0
+  (1...nums.length).each do |i|
+    temp = target - nums[i]
+    if hash.key?(temp)
+      result << hash[temp]
+      result << i
+      return result
+    else
+      hash[nums[i]] = i
+    end
+  end
+  result
+end
