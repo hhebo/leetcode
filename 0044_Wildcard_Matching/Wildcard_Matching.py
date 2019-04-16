d50cf8bcd451a465ba061955b739af040f3c7a20
hhebo <tmaclimt1@gmail.com>
2019-04-16 23:57:13 +0800

    q44 and q46

--- 0044_Wildcard_Matching/Wildcard_Matching.py
+++ 0044_Wildcard_Matching/Wildcard_Matching.py
@@ -1,1 +1,71 @@
 # -*- coding: utf-8 -*-
+
+"""
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+
+The matching should cover the entire input string (not partial).
+
+Note:
+
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+
+Example 1:
+
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+
+Input:
+s = "aa"
+p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+
+Example 3:
+
+Input:
+s = "cb"
+p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+
+Example 4:
+
+Input:
+s = "adceb"
+p = "*a*b"
+Output: true
+Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+
+Example 5:
+
+Input:
+s = "acdcb"
+p = "a*c?b"
+Output: false
+"""
+
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        len_s, len_p = len(s), len(p)
+        dp = [[False] * (len_p + 1) for _ in range(len_s + 1)]
+        dp[0][0] = True
+        for i in range(1, len_p + 1):
+            if p[i - 1] == '*':
+                dp[0][i] = dp[0][i - 1]
+        for i in range(1, len_s + 1):
+            for j in range(1, len_p + 1):
+                if p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+                else:
+                    dp[i][j] = (s[i - 1] == p[j - 1] or p[j - 1] == '?') and dp[i - 1][j - 1]
+        return dp[len_s][len_p]
