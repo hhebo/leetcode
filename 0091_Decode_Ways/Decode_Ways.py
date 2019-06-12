e7ef7aec31b3fb7a6e6db9eacc25528e589cbe1a
hhebo <tmaclimt1@gmail.com>
2019-06-12 23:04:07 +0800

    q91

---
+++ 0091_Decode_Ways/Decode_Ways.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+
+Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+Example 1:
+
+Input: "12"
+
+Output: 2
+
+Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+
+Example 2:
+
+Input: "226"
+
+Output: 3
+
+Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+"""
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        for i in range(1, len(dp)):
+            if s[i - 1] != '0':
+                dp[i] = dp[i - 1]
+            if i > 1 and (s[i - 2] == '1' or (s[i - 2] == '2' and s[i - 1] <= '6')):
+                dp[i] += dp[i - 2]
+        return dp[-1]
