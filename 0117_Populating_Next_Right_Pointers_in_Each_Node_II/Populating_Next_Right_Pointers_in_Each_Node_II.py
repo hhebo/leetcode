9ac8bcbe0450c7af0fb14dcc96baa2f028cca499
hhebo <tmaclimt1@gmail.com>
2019-07-23 23:58:46 +0800

    q117

---
+++ 0117_Populating_Next_Right_Pointers_in_Each_Node_II/Populating_Next_Right_Pointers_in_Each_Node_II.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+"""
+Given a binary tree
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Example:
+
+Input: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+
+Output: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":null,"right":null,"val":7},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"6","left":null,"next":null,"right":{"$ref":"5"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"6"},"val":1}
+
+Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B.
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root is None:
+            return
+        p = root.next
+        while p:
+            if p.left:
+                p = p.left
+                break
+            if p.right:
+                p = p.right
+                break
+            p = p.next
+        if root.right:
+            root.right.next = p
+        if root.left:
+            root.left.next = root.right if root.right else p
+        self.connect(root.right)
+        self.connect(root.left)
+        return root
