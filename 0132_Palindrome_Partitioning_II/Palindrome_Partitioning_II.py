103af6775f4d99f34a62e47938a4eda565a76fa4
hhebo <tmaclimt1@gmail.com>
2019-08-13 23:33:40 +0800

    q132

--- 0132_Palindrome_Partitioning_II/Palindrome_Partitioning_II.py
+++ 0132_Palindrome_Partitioning_II/Palindrome_Partitioning_II.py
@@ -23,7 +23,7 @@ class Solution:
         for i in range(len(s)):
             dp[i] = i
             for j in range(i + 1):
-                if s[i] == s[j] and (((i - j) < 2) or p[j + 1][i - 1]):
+                if s[i] == s[j] and (i - j < 2 or p[j + 1][i - 1]):
                     p[j][i] = True
                     dp[i] = 0 if j == 0 else min(dp[i], dp[j - 1] + 1)
         return dp[len(s) - 1]
